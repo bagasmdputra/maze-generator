66fd9551ecaeeb5d5172f23643f755b196da7f62
Bagaskoro Meyca <bagas_mdp@yahoo.com>
2018-05-15 01:27:42 +0700

    maze 3 4 done

--- Maze test/src/pattern.java
+++ Maze test/src/pattern.java
@@ -10,6 +10,11 @@ public class pattern {
 		printMaze1(n);
 		System.out.println();
 		printMaze2(n);
+		System.out.println();
+		printMaze3(n);
+		System.out.println();
+		printMaze4(n);
+		
 		//once finished
 		reader.close();
 
@@ -43,16 +48,17 @@ public class pattern {
 	}
 	
 	public static void	printMaze2(int size) {
-		int half = size/2;
 		for(int i = 0; i < size; i++) {
 			for(int j = 0; j < size; j++) {
-				if(
-						
-						(j%2==0 && i>=j && i<size-j) ||
-						(j%2==0 && i>=size-j && i<j) ||
-						(i%2==0 && j>=size-i && j<i+1) ||
-						(i%2==0 && j>=i+2 && j<size-i) ||
-						(j%2==0 && i==j-1 && j<half)
+				if(						
+						//left
+						(j%2==0 && i>=j-1 && i<size-j) ||
+						//right
+						(j%2==0 && i>=size-j && i<j+1) ||
+						//bottom
+						(i%2==0 && j>=size-i && j<i) ||
+						//top
+						(i%2==0 && j>=i+2 && j<size-i)
 						) {
 					System.out.print('@');
 				}else {
@@ -66,10 +72,53 @@ public class pattern {
 	}
 	
 	public static void	printMaze3(int size) {
-		int half = size/2;
 		for(int i = 0; i < size; i++) {
 			for(int j = 0; j < size; j++) {
+				if(
+						//left
+						(j%2==0 && i>=j-1 && i<size-j) ||
+						//right
+						(j%2==0 && i>=size-j && i<j+2) ||
+						//bottom
+						(i%2==0 && j>=size-i && j<i-1) ||
+						//top
+						(i%2==0 && j>=i+2 && j<size-i)
+						
+						) {
+					System.out.print('@');
+				}else {
+					System.out.print(' ');
+				}
+			}
+			System.out.println();
 				
+		}
+	}
+	
+	public static void	printMaze4(int size) {
+		for(int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++) {
+				if(
+						//vertical odd
+						(j%4==0 && (j==0 || (i!=0 && i>1)) && i<size-j) ||
+						//vertical even
+						(j%4==2 && (i<size-j || (j==size-1))) ||
+						
+						//vertical odd
+						(i%4==0 && (i==0 || (j!=0 && j+1>size-i)) && (i==0 || j<size-2)) ||
+						//vertical even
+						(i%4==2 && (j+1>size-i || (i==size-1))) 
+						
+						//bottom
+//						(i%2==0 && j>=size-i && j<i-1) 
+//						//horizontal
+//						(i%2==0 && j>=i+2 && j<size-i)
+						
+						) {
+					System.out.print('@');
+				}else {
+					System.out.print(' ');
+				}
 			}
 			System.out.println();
 				
